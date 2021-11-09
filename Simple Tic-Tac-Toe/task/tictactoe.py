de911166bea0e4bd9e393e313c8e44019df92f73
Joshua Meis <67470381+DireMouseRat@users.noreply.github.com>
2021-11-08 20:05:56 -0600

    Reduced line length and added exception handling. Refactored.

--- Simple Tic-Tac-Toe/task/tictactoe.py
+++ Simple Tic-Tac-Toe/task/tictactoe.py
@@ -3,9 +3,10 @@ class Game:
         self.x, self.o = 'X', 'O'
         self.current_player = self.x
         self.winner = str()
-        self.grid_size = 3
-        self.top_bottom_borders = '-' * (2 * self.grid_size + 3)
-        self.grid = [[' ' for _ in range(0, self.grid_size)] for _ in range(0, self.grid_size)]
+        self.size = 3
+        self.border_dashes = '-' * (2 * self.size + 3)
+        self.grid = [[' ' for _ in range(0, self.size)]
+                     for _ in range(0, self.size)]
         self.start_game()
 
     def start_game(self):
@@ -28,10 +29,10 @@ class Game:
         return False
 
     def print_game_grid(self):
-        print(self.top_bottom_borders)
-        for i in range(0, self.grid_size):
+        print(self.border_dashes)
+        for i in range(0, self.size):
             print("| " + ' '.join([p for p in self.grid[i]]) + " |")
-        print(self.top_bottom_borders)
+        print(self.border_dashes)
 
     def player_wins(self, p):
         return any([self.horizontal(p), self.vertical(p), self.diagonal(p)])
@@ -40,43 +41,43 @@ class Game:
         return any([all([cell == p for cell in row]) for row in self.grid])
 
     def vertical(self, p):
-        return any([all([self.grid[j][i] == p for j in range(0, self.grid_size)]) for i in range(0, self.grid_size)])
+        return any([all([self.grid[j][i] == p for j in range(0, self.size)])
+                    for i in range(0, self.size)])
 
     def diagonal(self, p):
-        left_to_right = all([self.grid[i][i] == p for i in range(0, self.grid_size)])
-        right_to_left = all([self.grid[i][self.grid_size - i - 1] == p for i in range(0, self.grid_size)])
+        left_to_right = all([self.grid[i][i] == p
+                             for i in range(0, self.size)])
+        right_to_left = all([self.grid[i][self.size - i - 1] == p
+                             for i in range(0, self.size)])
         return any([left_to_right, right_to_left])
 
     def empty_spaces(self):
         return any([any(cell == ' ' for cell in row) for row in self.grid])
 
     def take_turn(self):
-        valid = False
-        while not valid:
-            cell = input("Enter the coordinates: ").replace(' ', '')
-            if cell.isnumeric():
-                try:
-                    a, b = int(cell[0]), int(cell[1])
-                except IndexError:
-                    continue
-                if self.out_of_range(a, b):
-                    print("Coordinates should be from 1 to 3!")
-                elif self.occupied_cell(a, b):
+        next_turn = False
+        while not next_turn:
+            coordinates = input("Enter the coordinates: ")
+            try:
+                row, column = [int(num) - 1 for num in coordinates.split()]
+                if self.occupied_cell(row, column):
                     print("This cell is occupied! Choose another one!")
                 else:
-                    self.apply_cell(a, b)
-                    valid = True
-            else:
+                    self.apply_cell(row, column)
+                    next_turn = True
+            except IndexError:
+                print("Coordinates should be from 1 to 3!")
+            except ValueError:
                 print("You should enter numbers!")
 
-    def occupied_cell(self, a, b):
-        return self.grid[a - 1][b - 1] != ' '
+    def occupied_cell(self, row, column):
+        return self.grid[row][column] != ' '
 
-    def out_of_range(self, a, b):
-        return any([a < 1, a > self.grid_size, b < 1, b > self.grid_size])
+    def out_of_range(self, row, column):
+        return all([0 <= row < self.size, 0 <= column < self.size])
 
-    def apply_cell(self, a, b):
-        self.grid[a - 1][b - 1] = self.current_player
+    def apply_cell(self, row, column):
+        self.grid[row][column] = self.current_player
 
     def next_turn(self):
         self.current_player = 'O' if self.current_player == 'X' else 'X'
