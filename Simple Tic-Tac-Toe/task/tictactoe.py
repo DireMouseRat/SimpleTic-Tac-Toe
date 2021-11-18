ed03668d47f0494c626348866d69477e44f601cb
Joshua Meis <joshuadmeis@gmail.com>
2021-11-18 13:59:34 -0600

    Made text friendlier and randomized starting player.

--- Simple Tic-Tac-Toe/task/tictactoe.py
+++ Simple Tic-Tac-Toe/task/tictactoe.py
@@ -1,7 +1,11 @@
+import random
+
+
 class Game:
     def __init__(self):
+        random.seed()
         self.x, self.o = 'X', 'O'
-        self.current_player = self.x
+        self.current_player = random.choice([self.x, self.o])
         self.winner = str()
         self.size = 3
         self.border_dashes = '-' * (2 * self.size + 3)
@@ -10,11 +14,19 @@ class Game:
         self.start_game()
 
     def start_game(self):
+        self.print_starting_text()
         while not self.game_over():
             self.print_game_grid()
             self.take_turn()
             self.next_turn()
         self.declare_winner()
+
+    def print_starting_text(self):
+        print("=== Welcome to Tic Tac Toe! ===\n")
+        print("Enter a row number and a column number to place your mark")
+        print(f"For example, the top right corner is 1 {self.size}")
+        print("The first player to make a straight line wins!")
+        print(f"{self.current_player} will go first\n")
 
     def game_over(self):
         if self.player_wins(self.x):
@@ -28,8 +40,24 @@ class Game:
             return True
         return False
 
+    def take_turn(self):
+        next_turn = False
+        while not next_turn:
+            coordinates = input(f"Place an {self.current_player} at: ")
+            try:
+                row, column = [int(num) - 1 for num in coordinates.split()]
+                if self.occupied_cell(row, column):
+                    print("This cell is occupied.")
+                else:
+                    self.apply_cell(row, column)
+                    next_turn = True
+            except IndexError:
+                print(f"Coordinates must be two numbers between 1 and {self.size}.")
+            except ValueError:
+                print("No letters or special characters are allowed.")
+
     def print_game_grid(self):
-        print(f"It is {self.current_player}'s turn")
+        # print(f"It is {self.current_player}'s turn")
         print(self.border_dashes)
         for i in range(0, self.size):
             print("| " + ' '.join([p for p in self.grid[i]]) + " |")
@@ -54,22 +82,6 @@ class Game:
 
     def empty_spaces(self):
         return any([any(cell == ' ' for cell in row) for row in self.grid])
-
-    def take_turn(self):
-        next_turn = False
-        while not next_turn:
-            coordinates = input("Enter the coordinates: ")
-            try:
-                row, column = [int(num) - 1 for num in coordinates.split()]
-                if self.occupied_cell(row, column):
-                    print("This cell is occupied! Choose another one!")
-                else:
-                    self.apply_cell(row, column)
-                    next_turn = True
-            except IndexError:
-                print("Coordinates should be from 1 to 3!")
-            except ValueError:
-                print("You should enter numbers!")
 
     def occupied_cell(self, row, column):
         return self.grid[row][column] != ' '
